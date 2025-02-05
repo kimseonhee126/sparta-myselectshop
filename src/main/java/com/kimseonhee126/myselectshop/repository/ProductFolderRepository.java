dbe0c9ff20f1f8fe45a4acec29ecb6bb921fe7fe
kimseonhee126 <kimseonhee126@gmail.com>
2025-02-05 23:45:42 +0900

    상품 폴더 설계

---
+++ src/main/java/com/kimseonhee126/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.kimseonhee126.myselectshop.repository;
+
+import com.kimseonhee126.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
